c8650e0e4efc8352781b38236e3aaf88ec19db6e
alan061997 <alan061997@gmail.com>
2018-05-11 05:50:43 -0500

    Version Alpha

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from RFID import *
 from BD import *
 
 def main():
-    srl = Sensor()
+    srl = RFID()
     srl.init_serial()
-    print "Leyendo..."
+    print "Leyendo...\n"
+    time.sleep(3)
+
+    while True:
+        time.sleep(2)
+        id = srl.get_id()
+        print "\nID: " + id
+
+        time.sleep(2)
+        print "\nRegistrando..."
+        bd = BD()
+        bd.insert_data(id)
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
