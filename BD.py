852e15447c982019f58644d59f2f45c7c4176249
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-05-11 14:43:35 -0500

    cambio de accesos a base de datos

--- BD.py
+++ BD.py
@@ -4,7 +4,7 @@ import MySQLdb
 class BD():
 
     def insert_data(self, id):
-        db = MySQLdb.connect( "localhost", "root", "1234", "RFID" )
+        db = MySQLdb.connect( "MYSQL7003.site4now.net", "a3b3f6_h1326", "h1326223", "db_a3b3f6_h1326" )
         query = "INSERT INTO Acceso (idUsuario, fechatiempo) VALUES ('%s', SYSDATE(), '%s')" % (id)
         cursor = db.cursor()
         try:
@@ -15,7 +15,7 @@ class BD():
         db.close()
 
     def select_data(self):
-        db = MySQLdb.connect( "localhost", "root", "1234", "RFID" )
+        db = MySQLdb.connect( "MYSQL7003.site4now.net", "a3b3f6_h1326", "h1326223", "db_a3b3f6_h1326" )
         query = "SELECT * FROM Acceso"
         cursor = db.cursor()
         try:
